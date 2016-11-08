9cf41eeb4cd095fbb6d4be1d53703e2410541ce6
mobiotgroup <mobiotgroup@gmail.com>
2016-11-08 06:50:26 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookblog::Application.config.secret_token = '07fb4dd65e0adcb9cd6ca305889cf51d73f1e79ad3fc0c09396cc896e6c43fc0dc4c6f9c6cdea493c44a2aa14c1ddfd5513554a49e5155e8dfab2ccb190f966b'
